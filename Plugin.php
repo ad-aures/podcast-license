327644655920f9888020e0b4c480c47b5763ad83
Yassine Doghri <yassine@doghri.fr>
2024-10-19 16:15:04 +0000

    feat: add manifest settings + logic to rssAfterChannel hook

--- Plugin.php
+++ Plugin.php
@@ -10,6 +10,16 @@ class AdAuresPodcastLicensePlugin extends BasePlugin
 {
     public function rssAfterChannel(Podcast $podcast, SimpleRSSElement $channel): void
     {
-        // YOUR CODE HERE
+        $license = $this->getPodcastSetting($podcast->id, 'podcast-license');
+
+        $licenseValue = match ($license) {
+            'CC-BY-4.0'       => 'CC-BY-4.0',
+            'CC-BY-NC-4.0'    => 'CC-BY-NC-4.0',
+            'CC-BY-NC-ND-4.0' => 'CC-BY-NC-ND-4.0',
+            'POD-V4V-1.0'     => 'POD-V4V-1.0',
+            default           => 'All rights reserved'
+        };
+
+        $channel->addChild('license', $licenseValue, 'https://podcastindex.org/namespace/1.0');
     }
 }
